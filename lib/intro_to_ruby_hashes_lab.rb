c7fdb6f3c374379421821055811a3d60a0fbf6cf
dashing-utility-e4d8 <eten.uket@yale.edu>
2021-05-30 19:12:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -10,7 +10,7 @@ end
 
 def pioneer
   pioneer = {
-    :name, 'Grace Hopper'
+    :name => 'Grace Hopper'
   }# return a hash with a key of :name and a corresponding value of 'Grace Hopper'
   return pioneer
 end
