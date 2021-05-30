dc71c3d15029be1252a1e2215d5a190c2af62f24
dashing-utility-e4d8 <eten.uket@yale.edu>
2021-05-30 19:13:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -15,8 +15,13 @@ def pioneer
   return pioneer
 end
 
-
-
 def id_hash_generator(number)
+  number = 8
+  hash = {
+    :number => number
+  }
   # return a hash with a key :id assigned to the provided number
 end
+
+
+
